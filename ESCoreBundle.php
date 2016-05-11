38a9709174dc7666b353f8e1f7dc3042eaf5bb83
Emil Świniarski <emil.swiniarski@marketingmatch.pl>
2016-05-11 11:43:21 +0200

    comment

--- ESCoreBundle.php
+++ ESCoreBundle.php
@@ -7,6 +7,13 @@ use Symfony\Bundle\AsseticBundle\AsseticBundle;
 
 class ESCoreBundle extends Bundle
 {
+    /**
+     * This is a hack method which allows register dependency bundles semi automatically
+     * usage in README
+     *
+     * @param $environment string - current environment e.g dev, test, prod
+     * @return array of bundle's instances
+     */
     public function registerBundles($environment) {
         $bundles = array(
             new AsseticBundle(),
